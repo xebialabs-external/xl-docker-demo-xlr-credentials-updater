a97c96171195210164cc713ab4ef51c09c3d7a24
Joris De Winne <joris.dewinne@gmail.com>
2018-07-03 16:13:14 -0700

    Fixing docker push

--- travis-ci.sh
+++ travis-ci.sh
@@ -4,4 +4,4 @@ image_name="xebialabsunsupported/xl-docker-demo-xlr-credentials-updater"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t $image_name:latest .
-docker push $image_name:$tag
+docker push $image_name:latest
